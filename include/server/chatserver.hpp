3192119b507275a989516c8a04082c06b2c4ca3e
dhb <2511425476@qq.com>
2024-09-04 14:41:53 +0800

    chatserver

---
+++ include/server/chatserver.hpp
@@ -0,0 +1,27 @@
+#ifndef CHATSERVER_H
+#define CHATSERVER_H
+
+#include <muduo/net/TcpServer.h>
+#include <muduo/net/EventLoop.h>
+using namespace muduo;
+using namespace muduo::net;
+using namespace std;
+
+class ChatServer
+{
+public:
+    ChatServer(EventLoop *loop,
+               const InetAddress &listenAddr,
+               const string &nameArg);
+    void start();
+
+private:
+    void onConnection(const TcpConnectionPtr &);
+    void onMessage(const TcpConnectionPtr &,
+                   Buffer *,
+                   Timestamp);
+    TcpServer _server;
+    EventLoop *_loop;
+};
+
+#endif
